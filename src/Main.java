614bc26b11226c8b9e9dce6930e44f75ae8b5707
Lisa <83810302+al-atro@users.noreply.github.com>
2022-04-27 20:44:46 +0200

    Final version

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,9 @@ import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.layout.*;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 import java.util.*;
 
@@ -22,21 +25,46 @@ public class Main extends Application {
     ComboBox<String> timeslot = new ComboBox<>();
     Button button = new Button("Add lecturer");
     Button button2 = new Button("Find room");
+    Button button3 = new Button("Find lecturer");
+    Button button4 = new Button("Find course");
+    Button button5 = new Button("Add to schedule");
+    Button button6 = new Button("Add course");
+    Button button7 = new Button("Assign lecturer");
+    Button button8 = new Button("Expected number of students");
+    Text welcome = new Text();
+    Text intro = new Text();
+    Text intro2 = new Text();
     void setArea(String s){area.setText(s);}
     void clearField(){field.setText("");}
     @Override
     public void start(Stage stage) {
         con.initArea();
         field.setOnAction(e->con.enterText(field.getText()));
-        VBox root = new VBox(courses,lecturer,rooms,timeslot,field,button,button2,area);
+        VBox root = new VBox(welcome,courses,lecturer,rooms,timeslot,intro,button2,button3,button4,intro2,field,button,button6,button5,button7,button8,area);
         lecturer.getItems().addAll(model.getLecturer());
+        lecturer.setPromptText("Lecturer");
         courses.getItems().addAll(model.getCourse());
+        courses.setPromptText("Course");
         rooms.getItems().addAll(model.getRoom());
+        rooms.setPromptText("Room");
         timeslot.getItems().addAll(model.getTimeslot());
+        timeslot.setPromptText("Timeslot");
         button.setOnAction(e->con.addLecturer(field.getText()));
         button2.setOnAction(e->con.findRoom(courses.getValue()));
-        Scene scene = new Scene(root, 500, 500);
-        stage.setTitle("JavaFX Demo");
+        button3.setOnAction(e->con.findLecturer(courses.getValue()));
+        button4.setOnAction(e->con.findCourse(timeslot.getValue()));
+        button5.setOnAction(e->con.assignTimeslot(timeslot.getValue(), courses.getValue(), rooms.getValue()));
+        button6.setOnAction(e->con.addCourse(field.getText()));
+        button7.setOnAction(e->con.assignLecturer(courses.getValue(), lecturer.getValue()));
+        button8.setOnAction(e->con.assignStud(courses.getValue(), field.getText()));
+        welcome.setText("Welcome to the Course Management System");
+        welcome.setFont(Font.font ("Verdana", 20));
+        intro.setText(model.introText());
+        intro.setFont(Font.font ("Verdana", 12));
+        intro2.setText(model.intro2Text());
+        intro2.setFont(Font.font ("Verdana", 12));
+        Scene scene = new Scene(root, 550, 600);
+        stage.setTitle("Course Management system");
         stage.setScene(scene);
         stage.show();
     }
@@ -69,6 +97,7 @@ class Controller{
         } else {
             model.addLecturer(s);
             view.lecturer.getItems().add(s);
+            view.setArea("Lecturer added:  "+s);
         }
     }
     void findRoom(String c){
@@ -77,12 +106,80 @@ class Controller{
         else view.setArea("Room: "+room);
     }
 
+    void findLecturer(String c){
+        String lecturer=model.findLecturer(c);
+        if(lecturer.equals(""))view.setArea("No Lecturer");
+        else view.setArea("The lecturer of this course is "+lecturer);
+    }
+
+    void findCourse(String c){
+        String course=model.findCourse(c);
+        if(course.equals(""))view.setArea("No course takes place at this timeslot");
+        else view.setArea("The course that takes place at this timeslot is "+course);
+    }
+
+    /*
+    void assignRoom(String course, String room){
+        if (Integer.parseInt(model.studInCourse(course)) > Integer.parseInt(model.studInRoom(room))){
+            model.addRoomsCourses(course, room);
+            view.setArea("The room is assigned. Warning: the expected amount of students is larger, \nthan the capacity of the room!");
+        } else {
+            view.setArea("The room is assigned.");
+            model.addRoomsCourses(course, room);}
+    }
+
+     */
+
+    void assignTimeslot(String timeslot, String course, String room){
+        if (
+               model.roomFromTimeslot(timeslot).contains(room)
+            ){
+            view.setArea("The timeslot cannot be assigned. \nTwo courses cannot take place in the same room at the same time.");
+            }
+        else if (
+                model.lecturerFromTimeslot(timeslot).contains(model.findLecturer(course))
+                ) {
+                view.setArea("The timeslot cannot be assigned. \nOne lecturer cannot give two lectures at the same time.");
+                }
+        else if(
+                (Integer.parseInt(model.studInCourse(course)) > Integer.parseInt(model.studInRoom(room)))
+        ){
+            model.addTimeslotCourse(timeslot, course, room);
+            view.setArea("The timeslot is assigned. Warning: the expected amount of students is larger, \nthan the capacity of the room!");
+        }
+        else {
+            model.addTimeslotCourse(timeslot, course, room);
+            view.setArea("The timeslot is assigned.");
+        }
+    }
+
+    void addCourse(String s){
+        if(model.hasCourse(s)){
+            view.setArea("Cannot insert course (repeat) "+s);
+        } else {
+            model.addCourseUser(s);
+            view.courses.getItems().add(s);
+            view.setArea("Course added:  "+s);
+        }
+    }
+
+    void assignLecturer(String course, String lecturer){
+        model.assignLecturer(course, lecturer);
+        view.setArea("Lecturer assigned to "+course+": "+lecturer);
+    }
+
+    void assignStud(String course, String stud){
+        model.assignStud(course, stud);
+        view.setArea("The expected amount of student in the course "+course+" is now "+stud);
+    }
+
+
 }
 
 class Model{
     MyDB db=new MyDB();
     Model(){
-
+/*
         db.cmd("drop table if exists lst1;");
         db.cmd("create table if not exists lst1 "+
                 "(fld1 integer primary key autoincrement, fld2 text);");
@@ -96,14 +193,14 @@ class Model{
 
         db.cmd("drop table if exists Courses;");
         db.cmd("create table if not exists Courses "+
-                "(name text, stud integer);");
-        addCourse("Software Development", "60");
-        addCourse("Essential Computing", "50");
-        addCourse("Genetics", "80");
+                "(id integer primary key autoincrement, name text, stud integer, lecturerid integer, foreign key (lecturerid) references Lecturer (id));");
+        addCourse("Software Development", "60", "Brad Pitt");
+        addCourse("Essential Computing", "50", "Cameron Diaz");
+        addCourse("Genetics", "80", "Jane Fonda");
 
         db.cmd("drop table if exists Rooms;");
         db.cmd("create table if not exists Rooms "+
-                "(name text, stud integer);");
+                "(id integer primary key autoincrement, name text, stud integer);");
         addRoom("1.1","150");
         addRoom("9.2","50");
         addRoom("5.5","20");
@@ -112,12 +209,33 @@ class Model{
 
         db.cmd("drop table if exists Timeslot;");
         db.cmd("create table if not exists Timeslot "+
-                "(name text);");
+                "(id integer primary key autoincrement, name text);");
         String[] days = {"Monday","Tuesday","Wednesday","Thursday","Friday"};
         for(String day:days){addTimeslot(day+" AM");addTimeslot(day+" PM");}
 
+        db.cmd("drop table if exists TimeslotCourses;");
+        db.cmd("create table if not exists TimeslotCourses "+
+                "(timeslotid integer not null, courseid integer not null, roomid integer, foreign key (timeslotid) references Timeslot (id), foreign key (courseid) references Courses (id), foreign key (roomid) references Rooms (id));");
+        addTimeslotCourse("Monday AM", "Software Development", "10.1");
+        addTimeslotCourse("Monday PM", "Genetics", "1.1");
+        addTimeslotCourse("Tuesday PM", "Essential Computing", "9.2");
+        addTimeslotCourse("Thursday AM", "Software Development", "10.1");
+        addTimeslotCourse("Thursday PM", "Genetics", "1.1");
+        addTimeslotCourse("Friday AM", "Essential Computing", "9.2");
+
+        /*
+        db.cmd("drop table if exists RoomsCourses;");
+        db.cmd("create table if not exists RoomsCourses "+
+                "(roomid integer, courseid integer, foreign key (roomid) references Rooms (id), foreign key (courseid) references Courses (id));");
+        addRoomsCourses("1.1", "Genetics");
+        addRoomsCourses("9.2", "Essential Computing");
+        addRoomsCourses("10.1", "Software Development");
+
+         */
 
     }
+
+
 
 
     void addLecturer(String s){  db.cmd("insert into Lecturer (name) values ('"+s+"');");}
@@ -129,19 +247,97 @@ class Model{
         return lst.size()>0;
         //return getLecturer().contains(s);
     }
+    boolean hasCourse(String s){
+        ArrayList<String> lst= db.query("select name from Courses where name = '"+s+"';","name");
+        System.out.println(lst);
+        return lst.size()>0;
+        //return getLecturer().contains(s);
+    }
+    boolean LecturerAssigned(String s){
+        ArrayList<String> lst= db.query("select Lecturer.name from Lecturer\n" +
+                "inner join Courses on Lecturer.id = Courses.lecturerid\n" +
+                "where Courses.name = '"+s+"'","name");
+        System.out.println(lst);
+        return lst.size()>0;
+        //return getLecturer().contains(s);
+    }
+    String getLecturerID(String name){
+        ArrayList<String> ids = db.query("select id from Lecturer where name = '"+name+"';","name");
+        if (ids.size()>0) {
+            return ids.get(0);
+        } else {return "";}
+    }
 
     void addRoom(String s,String stud){db.cmd("insert into Rooms (name,stud) values ('"+s+"',"+stud+");");}
     ArrayList<String> getRoom(){return db.query("select name from Rooms;","name");}
 
-    void addCourse(String s,String stud){ db.cmd("insert into Courses (name,stud) values ('"+s+"',"+stud+");");}
+    void addCourse(String s, String stud, String lecname){ db.cmd("insert into Courses (name,stud, lecturerid) values ('"+s+"',"+stud+",(select id from Lecturer where name = '"+lecname+"'));");}
     ArrayList<String> getCourse(){
         return db.query("select name from Courses;","name");
     }
 
+    void addCourseUser(String s){ db.cmd("insert into Courses (name) values ('"+s+"');");}
+
+    String studInCourse(String s){
+        ArrayList<String> lst= db.query("select stud from Courses where name = '"+s+"';","stud");
+        if (lst.size()>0) {
+            return lst.get(0);
+        } else {return "";}
+    }
+
+    String studInRoom(String s){
+        ArrayList<String> lst= db.query("select stud from Rooms where name = '"+s+"';","stud");
+        if (lst.size()>0) {
+            return lst.get(0);
+        } else {return "";}
+    }
+
+    ArrayList<String> roomFromTimeslot(String s){
+        ArrayList<String> lst= db.query("select Rooms.name from Rooms" +
+                " inner join TimeslotCourses on Rooms.id = TimeslotCourses.roomid" +
+                " inner join Timeslot on Timeslot.id = TimeslotCourses.timeslotid" +
+                " where Timeslot.name = '"+s+"'", "name");
+        return lst;
+    }
+
+    ArrayList<String> lecturerFromTimeslot(String s){
+        ArrayList<String> lst= db.query("select Lecturer.name from Lecturer\n" +
+                "inner join Courses on Lecturer.id = Courses.lecturerid\n" +
+                "inner join TimeslotCourses on Courses.id = TimeslotCourses.courseid\n" +
+                "inner join Timeslot on TimeslotCourses.timeslotid = Timeslot.id\n" +
+                "where Timeslot.name = '"+s+"'", "name");
+        return lst;
+    }
+
+    void addTimeslotCourse(String timeslot, String course, String room){db.cmd("insert into TimeslotCourses(timeslotid, courseid, roomid) values ((select id from Timeslot where name = '"+
+            timeslot+"'), (select id from Courses where name = '"+course+"'), (select id from Rooms where name = '"+room+"'))");}
+
+    void addRoomsCourses(String room, String course){db.cmd("insert into RoomsCourses(roomid, courseid) values ((select id from Rooms where name = '"+
+            room+"'), (select id from Courses where name = '"+course+"'))");}
+
+
     String findRoom(String c){
         ArrayList<String> lst= db.query(
-                "select Rooms.name from Rooms inner join Courses"
-                        +" where Courses.name = '"+c+"' and Rooms.stud > Courses.stud;","name");
+                "select Rooms.name from ((Courses inner join RoomsCourses on Courses.id = RoomsCourses.courseid)" +
+                        " inner join Rooms on Rooms.id = RoomsCourses.roomid) where Courses.name = '"+c+"'", "name");
+        System.out.println(lst);
+        if(lst.size()==0)return "";
+        else return lst.get(0);
+    }
+
+
+    String findLecturer(String c){
+        ArrayList<String> lst= db.query(
+                "select Lecturer.name from Lecturer inner join Courses on Courses.lecturerid = Lecturer.id where Courses.name = '"+c+"'","name");
+        System.out.println(lst);
+        if(lst.size()==0)return "";
+        else return lst.get(0);
+    }
+
+    String findCourse(String c){
+        ArrayList<String> lst= db.query(
+                "select Courses.name from ((Courses inner join TimeslotCourses on Courses.id = TimeslotCourses.courseid)" +
+                        " inner join Timeslot on Timeslot.id = TimeslotCourses.timeslotid) where Timeslot.name = '"+c+"'","name");
         System.out.println(lst);
         if(lst.size()==0)return "";
         else return lst.get(0);
@@ -154,11 +350,29 @@ class Model{
         return db.query("select name from Timeslot;","name");
     }
 
+    void assignLecturer(String course, String lecturer){
+        db.cmd("update Courses set lecturerid = (select id from Lecturer where name = '"+lecturer+"') where name = '"+course+"'");
+    }
+    void assignStud(String course, String stud){
+        db.cmd("update Courses set stud = "+stud+" where name = '"+course+"'");
+    }
+
     void add(String s){ // remember to sanitize your data!
         db.cmd("insert into lst1 (fld2) values ('"+s+"');");
     }
     ArrayList<String> get(){
         return db.query("select fld2 from lst1 order by fld1;","fld2");
+    }
+    public String introText(){
+        return " To find a room assigned to a course, choose a course and press \"Find room\". " +
+                "\n To find a lecturer assigned to the course, choose a course and press \"Find lecturer\"." +
+                "\n To check if a course is assigned to a timeslot, choose a timeslot and press \"Find course\".";
+    }
+
+    public String intro2Text(){
+        return " To add a lecturer to the system, type the lecturer's name and press \"Add lecturer\"." +
+                "\n To add a course to the schedule, choose course, a room and a timeslot" +
+                "\n and press \"Add to schedule\".";
     }
 }
 
